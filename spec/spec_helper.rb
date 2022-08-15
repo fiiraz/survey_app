40b9165ff44ed039ec65b3d63f9d8e8fdd620bcb
özgür <ozgurpir14@gmail.com>
2022-08-15 21:13:44 +0300

    add some rubocop configs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,7 @@
 # it.
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-require 'simplecov'
+require "simplecov"
 SimpleCov.start
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
